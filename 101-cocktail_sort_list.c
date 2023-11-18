ac1a77652a0079727bbb624b6b54959e2227d577
preston176 <prestonnyamweya@gmail.com>
2023-11-18 12:55:03 +0300

    101-cocktail sort list

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,73 +1,66 @@
+/* 101-cocktail_sort_list.c */
+
 #include "sort.h"
 
 /**
- * quick_sort - function sorts an array of integers
- *              in the ascending order using the Quick sort algorithm
- * @array: array
- * @size: array's size
- * Return: void
+ * cocktail_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                      using the Cocktail Shaker sort algorithm
+ * @list: Pointer to the doubly linked list
  */
-void quick_sort(int *array, size_t size)
+void cocktail_sort_list(listint_t **list)
 {
-	if (array == NULL || size < 2)
-		return;
+    int swapped;
+    listint_t *current;
 
-	quick_s(array, 0, size - 1, size);
+    if (list == NULL || *list == NULL)
+        return;
+
+    do {
+        swapped = 0;
+        for (current = *list; current->next != NULL; current = current->next)
+        {
+            if (current->n > current->next->n)
+            {
+                swap_nodes(list, current, current->next);
+                swapped = 1;
+                print_list(*list);
+            }
+        }
+
+        if (!swapped)
+            break;
+
+        swapped = 0;
+        for (; current->prev != NULL; current = current->prev)
+        {
+            if (current->n < current->prev->n)
+            {
+                swap_nodes(list, current->prev, current);
+                swapped = 1;
+                print_list(*list);
+            }
+        }
+    } while (swapped);
 }
 
 /**
- * partition - partition
- * @array: array
- * @lo: lower
- * @hi: higher
- * @size: array's size
- * Return: i
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to the doubly linked list
+ * @node1: First node to swap
+ * @node2: Second node to swap
  */
-int partition(int *array, int lo, int hi, size_t size)
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-	int i = lo - 1, j = lo;
-	int pivot = array[hi], aux = 0;
+    if (node1->prev != NULL)
+        node1->prev->next = node2;
+    else
+        *list = node2;
 
-	for (; j < hi; j++)
-	{
-		if (array[j] < pivot)
-		{
-			i++;
-			if (array[i] != array[j])
-			{
-				aux = array[i];
-				array[i] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
-		}
-	}
-	if (array[i + 1] != array[hi])
-	{
-		aux = array[i + 1];
-		array[i + 1] = array[hi];
-		array[hi] = aux;
-		print_array(array, size);
-	}
-	return (i + 1);
-}
+    if (node2->next != NULL)
+        node2->next->prev = node1;
 
-/**
- * quick_s - quick sort
- * @array: given array
- * @lo: lower
- * @hi:higher
- * @size: array's size
- * Return: void
- */
-void quick_s(int *array, int lo, int hi, size_t size)
-{
-	int pivot;
-
-	if (lo < hi)
-	{
-		pivot = partition(array, lo, hi, size);
-		quick_s(array, lo, pivot - 1, size);
-		quick_s(array, pivot + 1, hi, size);
-	}
+    node1->next = node2->next;
+    node2->prev = node1->prev;
+    node1->prev = node2;
+    node2->next = node1;
 }
